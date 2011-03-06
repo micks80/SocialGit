71f50a734d0a4b04fb2500655f5440eac2f6db16
Mick Jhutty <micks80@gmail.com>
2011-03-06 07:43:49 -0500

    committed

--- Exp.java
+++ Exp.java
@@ -1,9 +1,7 @@
 public class Exp{
 	public static void main(String[] ar){
-<<<<<<< HEAD
-	System.out.println("Experimentation Master");
-=======
-	System.out.println("Experimentation Trunk");
->>>>>>> ec2794fa579dcfd733fda4d309d3930c9b7e6369
+
+	System.out.println("Experimentation");
+
 }
 }
